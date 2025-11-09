e2b2c1213993a8aea7ec33842e54f58531e98b22
Zsombinszky <zsombi.studio@gmail.com>
2025-11-09 13:00:56 +0100

    clean start ready

---
+++ data.js
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    name: "Gucci logo T-shirt",
+    type: "t-shirt",
+    price: 189990,
+    img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTk6Fo40tktN6rNYiTf0ZSYgGhuCrhHXlcqmcupJ1BgozgvLQEVvlRRJhC-kHZ6xixIx_5XvN8lhc2Byu030p36gXI0Qqb-8ejOydU1kxsFZOeiDYtC0rWHKzdA7hgnajcS9fyICFDiQA&usqp=CAc",
+  },
+  {
+    name: "Versace Medusa hoodie",
+    type: "hoodie",
+    price: 279990,
+    img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQOlG90hnO0kBewQNd2GonRS0tW_rbuj4qppwrBkisnR9fC4Y4Cxg080MTQ9cEm4sVSp4bKL4MvRVrODQYLLTYhvFYVFV2Qb2UhkqnCBFzX4Ej8zUuJoERB-PL2eOaBsCmA4txdz1FYYNA&usqp=CAc",
+  },
+  {
+    name: "Dior Oblique shirt",
+    type: "shirt",
+    price: 399990,
+    img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQKMkG7jUDx5BUTD4cTxV9aXZY_TFZ6bCd4hrf30hurvuT8JWfKYABId1osTwsn_-Qkol8Fj-JYKSGynxpxvr07jcDpjqZDAnFgtMUnilctV9I8KvEf0TLgSrNBFTqqcrwaDk0zBOM&usqp=CAc",
+  },
+  {
+    name: "Prada Re-Nylon jacket",
+    type: "jacket",
+    price: 629990,
+    img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQp8cLfIeVbWgtz7gPQCTL13RrOJy7iZDy7kOJ1ejGxj13xGpB6ofjOtCAJGOPQEPsn23rzBJrGY35dKbgMvuugqopYiEX9URPZlYJHtrSEz6vbBpIPI-e436WnrrJxc2uJLrd8ww&usqp=CAc",
+  },
+  {
+    name: "Louis Vuitton monogram jeans",
+    type: "jeans",
+    price: 549990,
+    img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQSq3kNoeyoaNnaAiAwNUCAVTZm6wu4T5WMPD8UBdIIWHNWeAjlnpeQtSXL7XeJHHn1sxM3uQ2Nvb7_i7QlfiuKKtsbeL5a8n561IrHkHugvIgcNMeT4TGJC0lVZuBDwxvgQRtUDvk&usqp=CAc",
+  },
+  {
+    name: "Balenciaga Triple S sneakers",
+    type: "sneakers",
+    price: 329990,
+    img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTHHKKRUfIWv7RG4kP6k-u3forBm04As0sijMSGjyTfSFH-JW03QObVzOq2f_RFyzJPxRqyr5h48IeNmAfnQIQxBmMaVQkFA5mVQ85KSqfo25_CpmyKx-3oQJzh2Hy-6yJ0lNaYfnSG&usqp=CAc",
+  },
+  {
+    name: "Burberry check scarf",
+    type: "scarf",
+    price: 169990,
+    img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTMkM5jFST73G9iLAqUPaUWsTx5VdfNsUC837_UJtnGFLw9ZveUDJx3IjUffFe8TF1Dr8PNNyIwpCEjHCqHOUoPaWnNDrLy0ZnFE5YbdVqzG1BySguDBnyca0PQcXUIyRLdcySvDrY&usqp=CAc",
+  },
+  {
+    name: "Saint Laurent leather jacket",
+    type: "leather jacket",
+    price: 999990,
+    img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTsaHmJKuaM1se02eTJzI6aA1_rBaEhB65fJNwbxWjtvS8n2mmJGrbz9Gdptd-79wQz7vZ8a4HKZUWAzYPWYNej9PGrUukIoPtP91boIU2U72w87vtz0ER5FePHHOAzeuJdEU-4Pqg&usqp=CAc",
+  },
+  {
+    name: "Fendi FF logo cap",
+    type: "cap",
+    price: 129990,
+    img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRxWI786FWN7vDPx5ca0K6fH1F0yd7VbQAxeiPVtS_ZjjLy7Xa6s8suaBiUU3zUxag7eYmTCCAyfNOnFo9WmgczOcP90y30_tP78SzIMSQlzczokfoDkamuD9o5SbHC3L67Cxmn1g&usqp=CAc",
+  },
+  {
+    name: "Givenchy cocktail dress",
+    type: "dress",
+    price: 749990,
+    img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSpatO8H8cOlOUqFi0spwjllHl0bpEgKH-b_8xgJnJncH4C8eU6lISaPbkdp3wAiXFMuTq9LEXBm6A4kzyeTucM3rgsfiuCRQSEJYCwXcTFF_bgt98MLYcZLbMugg&usqp=CAc",
+  },
+];
---
+++ script.js
@@ -0,0 +1,3 @@
+import { products } from "./data.js";
+
+const rootElement = document.getElementById("root");
